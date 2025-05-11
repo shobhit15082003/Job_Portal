3c3be940d2fc47ea21f95173b7cddb8f956abf39
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-05-12 00:43:36 +0530

    search bar done

--- frontend/src/FindJobs/SearchBar.tsx
+++ frontend/src/FindJobs/SearchBar.tsx
@@ -1,10 +1,11 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import MultiInput from './MultiInput'
 import { dropdownData } from '../Data/JobsData'
-import { Divider } from '@mantine/core'
+import { Divider, RangeSlider } from '@mantine/core'
 
 const SearchBar = () => {
+  const [value, setValue] = useState<[number, number]>([1, 100]);
   return (
     <div className='flex px-5 py-8 '>
       {
@@ -14,6 +15,22 @@ const SearchBar = () => {
         <Divider size="sm" orientation='vertical' mr="xs" />
         </>)
       }
+    <div className='w-1/5 [&_.mantine-Slider-label]:!translate-y-10'>
+      <div className='flex text-sm justify-between '>
+        <div>Salary</div>
+        <div>&#8377;{value[0]} LPA - &#8377;{value[1]} LPA</div>
+      </div>
+      <RangeSlider size="xs" color='brightSun.4'  value={value}
+        labelTransitionProps={{
+          transition: 'skew-down',
+          duration: 150,
+          timingFunction: 'linear',
+        }}
+        
+      onChange={setValue} /> 
+    </div>   
+  
+
     </div>
   )
 }
--- frontend/src/Pages/FindJobs.tsx
+++ frontend/src/Pages/FindJobs.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchBar from '../FindJobs/SearchBar'
 import { Divider } from '@mantine/core'
 
 const FindJobs = () => {
+   
   return (
    <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
         <Divider size="sm" mx="md" />
