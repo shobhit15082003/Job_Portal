6b7ab1073957cae56ede3b60aaf5dd1dfe992ba9
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-05-23 04:57:24 +0530

    done for the day

---
+++ frontend/src/Pages/TalentProfilePage.tsx
@@ -0,0 +1,12 @@
+import { Divider } from '@mantine/core'
+import React from 'react'
+
+const TalentProfilePage = () => {
+  return (
+    <div className="min-h-[90vh] bg-mine-shaft-950 font-['poppins']">
+        <Divider size="xs" mx="md"/>
+    </div>
+  )
+}
+
+export default TalentProfilePage
