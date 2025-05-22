0a1552ca0970cea848f40e5f15d3ad6d236d2b13
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-05-23 03:39:42 +0530

    find job page complete

--- frontend/src/FindJobs/JobCard.tsx
+++ frontend/src/FindJobs/JobCard.tsx
@@ -2,36 +2,36 @@ import { Divider, Text } from '@mantine/core'
 import { IconBookmark, IconClockHour3 } from '@tabler/icons-react'
 import React from 'react'
 
-const JobCard = () => {
+const JobCard = (props:any) => {
   return (
-    <div className='bg-mine-shaft-900 p-4 w-72 flex flex-col gap-3 rounded-xl '>
+    <div className='bg-mine-shaft-900 p-4 w-72 flex flex-col gap-3 rounded-xl hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400  '>
       <div className='flex justify-between '>
         <div className='flex gap-2 items-center'>
           <div className='p-2 bg-mine-shaft-800 rounded-md '>
-            <img className='h-7' src='/Icons/Microsoft.png' alt=''/>
+            <img className='h-7' src={`/Icons/${props.company}.png`} alt=''/>
           </div>
           <div>
-            <div className='font-semibold'>Product Designer</div>
-            <div className='text-xs text-mine-shaft-300 '>Meta &#x2022; 25 Applicants</div>
+            <div className='font-semibold'>{props.jobTitle}</div>
+            <div className='text-xs text-mine-shaft-300 '>{props.company} &#x2022; {props.applicants} Applicants</div>
           </div>
         </div>
         <IconBookmark className='text-mine-shaft-300 cursor-pointer'/>
       </div>
       <div className='flex gap-2 [&>div]:py-1 [&>div]:px-2 [&>div]:bg-mine-shaft-800 [&>div]:text-bright-sun-400 [&>div]:rounded-lg text-xs'>
-        <div>Entry Level</div>
-        <div>Full Time</div>
-        <div>Remote</div>
+        <div>{props.experience}</div>
+        <div>{props.jobType} </div>
+        <div>{props.location}</div>
       </div>
       <Text className='!text-xs text-justify !text-mine-shaft-300 '  lineClamp={2}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, iusto dolor sit similique hic exercitationem numquam, in provident iure veniam aperiam eius, odio commodi possimus unde fugiat dolorem sint ipsum!
+        {props.description}
       </Text>
       <Divider size="xs" color='mineShaft.7' />
       <div className='flex justify-between '>
         <div className='font-semibold text-mine-shaft-200 '>
-          &#8377;24 LPA
+          &#8377;{props.package}
         </div>
         <div className='flex gap-1 text-xs items-center text-mine-shaft-400'>
-          <IconClockHour3 stroke={1.5} className='h-5 w-5 '/> 12 days ago
+          <IconClockHour3 stroke={1.5} className='h-5 w-5 '/> {props.postedDaysAgo} days ago
         </div>
       </div>
     </div>
--- frontend/src/FindJobs/Jobs.tsx
+++ frontend/src/FindJobs/Jobs.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import Sort from './Sort'
-import JobCard from './JobCard'
+import React from "react";
+import Sort from "./Sort";
+import JobCard from "./JobCard";
+import { jobList } from "../Data/JobsData";
 
 const Jobs = () => {
   return (
-    <div className='p-5'>
-      
-      <div className='flex justify-between'>
-        <div className='text-2xl font-semibold '>
-            Recommended Jobs
-        </div>
-        
-            <Sort/>
-       
-      </div>
-      <JobCard/>
-    </div>
-  )
-}
+    <div className="p-5">
+      <div className="flex justify-between">
+        <div className="text-2xl font-semibold ">Recommended Jobs</div>
 
-export default Jobs
+        <Sort />
+      </div>
+      <div className="mt-10 flex flex-wrap gap-5 ">
+        {jobList.map((job, index) => (
+          <JobCard key={index} {...job} />
+        ))}
+      </div>
+      {/* <JobCard/> */}
+    </div>
+  );
+};
+
+export default Jobs;
