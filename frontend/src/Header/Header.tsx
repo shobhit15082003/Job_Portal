36b6aa566e6b3b16dc2a5679a351a4793d465b71
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-05-31 04:00:50 +0530

    menu items created

--- frontend/src/Header/Header.tsx
+++ frontend/src/Header/Header.tsx
@@ -3,21 +3,20 @@ import { IconAnchor, IconAsset, IconBell, IconSettings } from "@tabler/icons-rea
 import avatarImage from '../assests/avatar-9.png';
 import NavLinks from "./NavLinks";
 import { useLocation } from "react-router-dom";
+import ProfileMenu from "./ProfileMenu";
 
 const Header = () => {
   const location =useLocation();
   return (
     location.pathname!="/signup" && location.pathname!="/login" ?<div className="w-full bg-mine-shaft-950 px-6 h-20 text-white flex justify-between items-center font-['poppins]">
-      <div className="flex gap-1 items-center text-bright-sun-400">
+      <div className="flex gap-1  items-center text-bright-sun-400">
         <IconAnchor className="h-8 w-8" stroke={2.5} />
         <div className="text-3xl font-semibold">JobHook</div>
       </div>
       <NavLinks/>
       <div className="flex items-center gap-3">
-        <div className="flex items-center gap-2">
-          <div>Shobhit</div>
-          <Avatar src={avatarImage} />
-        </div>
+     
+        <ProfileMenu/>
         <div className="bg-mine-shaft-900 p-1.5 rounded-full">
           <IconSettings stroke={1.5} />
         </div>
