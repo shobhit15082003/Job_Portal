1e34765054379ee228b577fc8fa6eaa622a94a62
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-06-03 09:37:01 +0530

    basic api

---
+++ backend/src/main/java/com/jobportal/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.jobportal.dto;
+
+
+import com.jobportal.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private String  id;
+    @NotBlank(message = "Name is null or blank")
+    private String  name;
+    @NotBlank(message = "Email is null or blank")
+    private String  email;
+    @NotBlank(message = "Password is null or blank")
+    private String password;
+    private AccountType accountType;
+
+    public User toEntity(){
+        return new User(this.id, this.name,this.email,this.password,this.accountType);
+    }
+}
---
+++ backend/src/main/java/com/jobportal/service/UserService.java
@@ -0,0 +1,8 @@
+package com.jobportal.service;
+
+import com.jobportal.dto.UserDTO;
+
+public interface UserService {
+    public UserDTO registerUser(UserDTO userDTO);
+
+}
