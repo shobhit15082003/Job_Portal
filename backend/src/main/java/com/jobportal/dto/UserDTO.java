5b279d64412de13a053f289a0c90cacc2e81fe23
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-06-04 10:50:18 +0530

    basic api done

--- backend/src/main/java/com/jobportal/dto/UserDTO.java
+++ backend/src/main/java/com/jobportal/dto/UserDTO.java
@@ -2,7 +2,9 @@ package com.jobportal.dto;
 
 
 import com.jobportal.entity.User;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,11 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
     private String  id;
-    @NotBlank(message = "Name is null or blank")
+    @NotBlank(message = "{user.name.absent}")
     private String  name;
-    @NotBlank(message = "Email is null or blank")
+    @NotBlank(message = "{user.email.absent}")
+    @Email(message = "{user.email.invalid}")
     private String  email;
-    @NotBlank(message = "Password is null or blank")
+    @NotBlank(message = "{user.password.absent}")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,15}$\n", message = "{user.password.invalid}")
     private String password;
     private AccountType accountType;
 
