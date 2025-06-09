d3bd7729ba8cfd5958b56993d1d244de68bafea2
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-06-09 07:10:22 +0530

    working on profile api integration

--- frontend/src/Component/Profile/Info.tsx
+++ frontend/src/Component/Profile/Info.tsx
@@ -9,17 +9,27 @@ import {
 } from "@tabler/icons-react";
 import SelectInput from "./SelectInput";
 import { hasLength, isEmail, useForm } from "@mantine/form";
+import { useDispatch, useSelector } from "react-redux";
+import { changeProfile } from "../../Slices/ProfileSlice";
+import { successNotification } from "../../Services/NotificationService";
 
 const Info = () => {
   const select = fields;
+  const dispatch =useDispatch();
+  const user=useSelector((state:any)=>state.user);
+  const profile=useSelector((state:any)=>state.profile);
   const [edit, setEdit] = useState(false);
   const handleClick = () => {
     if(!edit){
         setEdit(true);
+        form.setValues({jobTitle:profile.jobTitle,company:profile.company,location:profile.location});
     }
     else{
         setEdit(false);
-        console.log(form.getValues());
+        let updatedProfile={...profile,...form.getValues()};
+        dispatch(changeProfile(updatedProfile));
+        console.log(updatedProfile);
+        successNotification("Success","Profile Updated Successfully.");
     }
   };
 
@@ -31,9 +41,9 @@ const Info = () => {
   return (
     <>
       <div className="text-3xl font-semibold flex justify-between ">
-        Jarrod Wood
+        {user.name}
         <ActionIcon
-          onClick={() => handleClick}
+          onClick={handleClick}
           variant="subtle"
           color="brightSun.4"
           size="lg"
@@ -48,20 +58,20 @@ const Info = () => {
       {edit ? (
         <>
           <div className="flex gap-10 [&>*]:w-1/2">
-            <SelectInput {...select[0]} />
-            <SelectInput {...select[1]} />
+            <SelectInput form={form} name="jobTitle" {...select[0]} />
+            <SelectInput form={form} name="company" {...select[1]} />
           </div>
-          <SelectInput {...select[2]} />
+          <SelectInput form={form} name="location" {...select[2]} />
         </>
       ) : (
         <>
           <div className="text-xl flex gap-1 items-center">
             <IconBriefcase className="h-5 w-5" stroke={1.5} />
             {/* {props.role}{" "} */}
-            Software Emgineer &bull; Google
+            {profile.jobTitle} &bull; {profile.company}
           </div>
           <div className="flex gap-1 text-lg items-center text-mine-shaft-300">
-            <IconMapPin stroke={1.5} className="h-5 w-5 " /> New York, USA
+            <IconMapPin stroke={1.5} className="h-5 w-5 " /> {profile.location}
           </div>
         </>
       )}
