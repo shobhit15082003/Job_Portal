e0322af7621c2e3bce964211a81d0378e3df7351
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-06-21 02:55:45 +0530

    filter done

--- frontend/src/Component/FindTalent/SearchBar.tsx
+++ frontend/src/Component/FindTalent/SearchBar.tsx
@@ -1,47 +1,72 @@
+import React, { useState } from "react";
 
-import React, { useState } from 'react'
+import { Divider, Input, RangeSlider } from "@mantine/core";
+import MultiInput from "../FindJobs/MultiInput";
 
-import { Divider, Input, RangeSlider } from '@mantine/core'
-import MultiInput from '../FindJobs/MultiInput';
-
-import { IconUserCircle } from '@tabler/icons-react';
-import { searchFields } from '../../Data/TalentData';
+import { IconUserCircle } from "@tabler/icons-react";
+import { searchFields } from "../../Data/TalentData";
+import { useDispatch } from "react-redux";
+import { updateFilter } from "../../Slices/FilterSlice";
 
 const SearchBar = () => {
-  const [value, setValue] = useState<[number, number]>([1, 100]);
+  const [value, setValue] = useState<[number, number]>([0, 50]);
+  const [name, setName] = useState("");
+  const dispatch=useDispatch();
+  const handleChange = (name: any, event: any) => {
+    if(name=="exp"){
+      dispatch(updateFilter({exp:event}));
+    }else{
+      dispatch(updateFilter({name:event.target.value}));
+      setName(event.target.value);
+    }
+  };
   return (
-    <div className='flex px-5 py-8 items-center !text-mine-shaft-100 '>
-      <div className='flex items-center'>
-        <div className='text-bright-sun-400 bg-mine-shaft-900 rounded-full p-1 mr-2'><IconUserCircle size={20} /></div>
-        <Input variant='unstyled' placeholder='Talent Name' className='[&_input]:!placeholder-mine-shaft-300  '/>
-        
-       
-      </div>
-      {
-        searchFields.map((item,index)=><><div key={index} className='w-1/5'>
-          <MultiInput {...item}/>
+    <div className="flex px-5 py-8 items-center !text-mine-shaft-100 ">
+      <div className="flex items-center">
+        <div className="text-bright-sun-400 bg-mine-shaft-900 rounded-full p-1 mr-2">
+          <IconUserCircle size={20} />
         </div>
-        <Divider size="sm" orientation='vertical' mr="xs" />
-        </>)
-      }
-    <div className='w-1/5 [&_.mantine-Slider-label]:!translate-y-10'>
-      <div className='flex text-sm justify-between '>
-        <div>Salary</div>
-        <div>&#8377;{value[0]} LPA - &#8377;{value[1]} LPA</div>
+        <Input
+          defaultValue={name}
+          onChange={(e) => handleChange("name", e)}
+          variant="unstyled"
+          placeholder="Talent Name"
+          className="[&_input]:!placeholder-mine-shaft-300  "
+        />
       </div>
-      <RangeSlider size="xs" color='brightSun.4'  value={value}
-        labelTransitionProps={{
-          transition: 'skew-down',
-          duration: 150,
-          timingFunction: 'linear',
-        }}
-        
-      onChange={setValue} /> 
-    </div>   
-  
-
+      {searchFields.map((item, index) => (
+        <React.Fragment key={index}>
+          <div className="w-1/5">
+            <MultiInput {...item} />
+          </div>
+          <Divider size="sm" orientation="vertical" mr="xs" />
+        </React.Fragment>
+      ))}
+      <div className="w-1/5 [&_.mantine-Slider-label]:!translate-y-10">
+        <div className="flex text-sm justify-between ">
+          <div>S=Experience (Years)</div>
+          <div>
+            {value[0]} - {value[1]}
+          </div>
+        </div>
+        <RangeSlider
+          onChangeEnd={(e) => handleChange("exp", e)}
+          size="xs"
+          color="brightSun.4"
+          max={50}
+          min={0}
+          value={value}
+          minRange={1}
+          labelTransitionProps={{
+            transition: "skew-down",
+            duration: 150,
+            timingFunction: "linear",
+          }}
+          onChange={setValue}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
--- frontend/src/Component/FindTalent/Talents.tsx
+++ frontend/src/Component/FindTalent/Talents.tsx
@@ -1,32 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import Sort from '../FindJobs/Sort'
+import React, { useEffect, useState } from "react";
+import Sort from "../FindJobs/Sort";
 
-import TalentCard from './TalentCard';
-import { getAllProfile } from '../../Services/ProfileService';
+import TalentCard from "./TalentCard";
+import { getAllProfile } from "../../Services/ProfileService";
+import { useDispatch, useSelector } from "react-redux";
+import { resetFilter } from "../../Slices/FilterSlice";
 
 const Talents = () => {
-  const [talents,setTalents]=useState<any>();
-  useEffect(()=>{
-    getAllProfile().then((res)=>{
-      setTalents(res);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  },[])
+  const [talents, setTalents] = useState<any>();
+  const filter = useSelector((state: any) => state.filter);
+  const [filteredTalents, setFilteredTalents] = useState<any>([]);
+  const dispatch=useDispatch();
+  useEffect(() => {
+    dispatch(resetFilter());
+    getAllProfile()
+      .then((res) => {
+        setTalents(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  useEffect(() => {
+    let filterTalent = talents;
+    setFilteredTalents(talents);
+    if (filter.name) {
+      filterTalent = filterTalent.filter((talent: any) =>
+        talent.name.toLowerCase().includes(filter.name.toLowerCase())
+      );
+      if (filter["Job Title"] && filter["Job Title"].length > 0) {
+        filterTalent = filterTalent.filter((talent: any) =>
+          filter["Job Title"]?.some((title: any) =>
+            talent.jobTitle.toLowerCase().includes(title.toLowerCase())
+          )
+        );
+      }
+      if(filter.Location && filter.Location.length>0){
+        filterTalent=filterTalent.filter((talent:any)=>filter.Location?.some((location:any)=>talent.location.toLowerCase().includes(location.toLowerCase())));
+      }
+      if(filter.Skills && filter.Skills.length>0){
+        filterTalent=filterTalent.filter((talent:any)=>filter.Skills?.some((skill:any)=>talent.skills?.some((talentSkill:any)=>talentSkill.toLowerCase().includes(skill.toLowerCase))));
+      }
+      if(filter.exp && filter.exp.length>0){
+        filterTalent=filterTalent.filter((talent:any)=>filter.exp[0]<=talent.totalExp && talent.totalExp<=filter.exp[1]);
+      }
+      
+      setFilteredTalents(filterTalent);
+    }
+  }, [filter, talents]);
   return (
-     <div className="p-5">
+    <div className="p-5">
       <div className="flex justify-between">
         <div className="text-2xl font-semibold ">Talents</div>
         <Sort />
       </div>
       <div className="mt-10 flex flex-wrap gap-5 justify-around">
-        {
-          talents.map((talent:any,index:any)=><TalentCard key={index} {...talent}/>)
-        }
-      </div>
+        {filteredTalents.length?filteredTalents.map((talent: any, index: any) => (
+          <TalentCard key={index} {...talent} />
+        )):<div className="text-xl font-semibold">No Talents found</div>
       
+      }
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Talents
+export default Talents;
