3cf503fa95b9030b6007ba6da9a4d183e18fd48f
shobhit15082003 <shobhit.2201140ec@iiitbh.ac.in>
2025-06-26 00:48:19 +0530

    another page made responsive

--- frontend/src/Component/TalentProfile/ExpCard.tsx
+++ frontend/src/Component/TalentProfile/ExpCard.tsx
@@ -5,7 +5,7 @@ import { formatDate } from '../../Services/UtilitiesService'
 const ExpCard = (props:any) => {
   return (
     <div className='flex flex-col gap-2 '>
-        <div className='flex justify-between '>
+        <div className='flex justify-between gap-2 flex-wrap'>
         <div className='flex gap-2 items-center'>
           <div className='p-2 bg-mine-shaft-800 rounded-md '>
             <img className='h-7' src={`/Icons/${props.company}.png`} alt=''/>
@@ -19,7 +19,7 @@ const ExpCard = (props:any) => {
             {formatDate(props.startDate)} - {formatDate(props.endDate)}
         </div>
       </div>
-      <div className='text-sm text-mine-shaft-300 text-justify '>
+      <div className='text-sm xs-mx:text-xs text-mine-shaft-300 text-justify '>
         {props.description}
       </div>
     </div>
--- frontend/src/Component/TalentProfile/Profile.tsx
+++ frontend/src/Component/TalentProfile/Profile.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import avatar from "../../assests/avatar-9.png";
 import { IconBriefcase, IconMapPin } from "@tabler/icons-react";
-import { Button, Divider } from "@mantine/core";
+import { Avatar, Button, Divider } from "@mantine/core";
 import ExpCard from "./ExpCard";
 import CertiCard from "./CertiCard";
 import { useParams } from "react-router-dom";
 import { getProfile } from "../../Services/ProfileService";
+import { useMediaQuery } from "@mantine/hooks";
 
 const Profile = (props: any) => {
   const { id } = useParams();
   const [profile, setProfile] = useState<any>({});
+  const matches=useMediaQuery('(max-width:475px)');
   useEffect(() => {
-    window.scrollTo(0,0);
+    window.scrollTo(0, 0);
     getProfile(id)
       .then((res) => {
         setProfile(res);
@@ -19,33 +21,51 @@ const Profile = (props: any) => {
       .catch((err) => {
         console.log(err);
       });
-  },[id]);
+  }, [id]);
   return (
-    <div className="w-2/3">
+    <div className="w-2/3 lg-mx:w-full">
       <div className="relative">
-        <img className="rounded-t-2xl " src="/Profile/banner.jpg" alt="" />
-        <img
-          className="rounded-full w-48 h-48 -bottom-1/3 absolute left-3 border-mine-shaft-950 border-8"
+        {/* <img className="rounded-t-2xl " src="/Profile/banner.jpg" alt="" /> */}
+        {/* <img
+           className="absolute !w-48 !h-48 md-mx:!w-40 md-mx:!h-40 sm-mx:!w-36 sm-mx:!h-36 xs-mx:!w-32 xs-mx:!h-32  border-mine-shaft-950 border-8 rounded-full"
            src={profile.picture?`data:image/jpeg;base64,${profile.picture}`:avatar}
           alt=""
+        /> */}
+        <img
+          className="rounded-t-2xl xl-mx:h-40   xs-mx:h-32 lg-mx:w-full"
+          src="/Profile/banner.jpg"
+          alt=""
         />
+        <div className="absolute flex items-center justify-center -bottom-1/3  md-mx:-bottom-10 sm-mx:-bottom-16  left-6 ">
+          <Avatar
+            className="!w-48 !h-48 md-mx:!w-40 md-mx:!h-40 sm-mx:!w-36 sm-mx:!h-36 xs-mx:!w-32 xs-mx:!h-32  border-mine-shaft-950 border-8 rounded-full"
+            src={
+              profile?.picture
+                ? `data:image/jpeg;base64,${profile?.picture}`
+                : avatar
+            }
+            alt=""
+          />
+        </div>
       </div>
       <div className="px-3 mt-16 ">
-        <div className="text-3xl font-semibold flex justify-between ">
+        <div className="text-3xl xs-mx:text-2xl font-semibold flex justify-between ">
           {profile?.name}{" "}
-          <Button color="brightSun.4" variant="light">
+          <Button size={matches?"sm":"md"} color="brightSun.4" variant="light">
             Message
           </Button>{" "}
         </div>
-        <div className="text-xl flex gap-1 items-center">
-          <IconBriefcase className="h-5 w-5" stroke={1.5} /> {profile?.jobTitle} &bull;{" "}
-          {profile?.company}
+        <div className="text-xl flex gap-1 items-center xs-mx:text-base">
+          <IconBriefcase className="h-5 w-5" stroke={1.5} /> {profile?.jobTitle}{" "}
+          &bull; {profile?.company}
         </div>
-        <div className="flex gap-1 text-lg items-center text-mine-shaft-300">
+        <div className="flex gap-1 text-lg items-center text-mine-shaft-300  xs-mx:text-base">
           <IconMapPin stroke={1.5} className="h-5 w-5 " /> {profile?.location}
         </div>
-        <div className="flex gap-1 text-lg items-center text-mine-shaft-300">
-          <IconBriefcase stroke={1.5} className="h-5 w-5 " />Experience {profile?.totalExp} {profile?.totalExp>1?"Years":"Year"}
+        <div className="flex gap-1 text-lg items-center text-mine-shaft-300  xs-mx:text-base">
+          <IconBriefcase stroke={1.5} className="h-5 w-5 " />
+          Experience {profile?.totalExp}{" "}
+          {profile?.totalExp > 1 ? "Years" : "Year"}
         </div>
       </div>
       <Divider mx="xs" size="xs" my="xl" />
